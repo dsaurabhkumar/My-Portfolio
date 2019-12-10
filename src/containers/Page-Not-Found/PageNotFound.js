a9eae6caf689e15fd3f5723267c09e5d3e902a99
Saurabh Kumar <dsaurabh.kumar1288@gmail.com>
2019-12-10 14:34:54 +0530

    PageNotFound Component updated

--- src/containers/Page-Not-Found/PageNotFound.js
+++ src/containers/Page-Not-Found/PageNotFound.js
@@ -2,15 +2,13 @@ import React from 'react';
 
 function PageNotFound() {
     return (
-        <div className="container">
-            <div className="pageNotFound">
+            <div className="container pageNotFound">
                 <h1>404</h1>
                 <div>
                     <h1>Page Not Found !!!</h1>
                     <p>Looks like the page you are looking for is no longer available</p>
                 </div>
             </div>
-        </div>
     )
 }
 export default PageNotFound
